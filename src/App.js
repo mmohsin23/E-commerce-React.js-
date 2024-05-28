413a7e4f388dd9455101743d03b14384b9d23ac9
muhammadmohsin1999 <muhammad.mohsin@centric.ae>
2024-05-28 11:57:40 +0500

    Routing and Declaring Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Navbar from './components/navbar/navbar';
+import ShopPage from './pages/shopPage';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -7,10 +8,13 @@ function App() {
     <div>
       <BrowserRouter>
         <Navbar />
+        <ShopPage />
         <Routes>
           <Route path="/" element={<shopPage/>} />
           <Route path="/login" element={<loginSignup/>} />
           <Route path="/shop " element={<shopCategory/>} />
+          <Route path="/about" element={<aboutUs/>} />
+          <Route path="/contact" element={<contactUs/>} />
           <Route path="/product" element={<products/>}>
             <Route path=':productId' element={<product/>} />
           </Route>
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './hero.scss'
+const Hero = () => {
+  return (
+    <div className='HeroComponent'>
+        Han sab theek hain,
+        Mein hero se bolraha hun
+        
+    </div>
+  )
+}
+
+export default Hero
