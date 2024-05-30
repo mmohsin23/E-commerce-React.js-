f74d07e26f45cbd13f344883332f314f4bd1dd01
muhammadmohsin1999 <muhammad.mohsin@centric.ae>
2024-05-30 23:37:10 +0500

    created product component

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './product.scss'
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
 const products = () => {
   return (
-    <div>products</div>
+    <div className='ProductCard mt-3'>
+      <div className="imgHover">
+        <img src="/Assets/media-01.png" alt="" />
+        <button>View Details</button>
+      </div>
+      <div className="productInfo">
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={farStar} />
+        <FontAwesomeIcon icon={farStar} />
+        <h2>Product Name</h2>
+        <h3 className='original'>
+          <span>
+              Rs 6,500.00
+          </span>
+          Rs 4,500.00
+        </h3>
+      </div>
+
+
+    </div>
+
   )
 }
 
--- src/pages/shopPage.jsx
+++ src/pages/shopPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import ProductIcon from '../pages/products'
 
 const shopPage = () => {
   return (
-    <div>
+    <div style={{paddingBottom: 100+'px'}}>
       <Hero />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-sm-6">
+            <ProductIcon />
+          </div>
+          <div className="col-lg-4 col-sm-6">
+            <ProductIcon />
+          </div>
+          <div className="col-lg-4 col-sm-6">
+            <ProductIcon />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
